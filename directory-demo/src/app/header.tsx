3428ccbbf50c7a1a5802332095ceb085cda6cfb0
Alex Mickelson <alexmickelson96@gmail.com>
2024-10-22 08:26:19 -0600

    some styling

--- directory-demo/src/app/header.tsx
+++ directory-demo/src/app/header.tsx
@@ -6,10 +6,21 @@ export default function Header({ username }: { username: string }) {
   const auth = useAuth();
 
   const isLoggedIn = username || auth.isAuthenticated;
+
   return (
-    <div>
-      {!isLoggedIn && <button onClick={() => auth.signinRedirect()}>Login</button>}
-      {isLoggedIn && <button onClick={() => auth.signoutRedirect()}>Logout</button>}
+    <div className="bg-emerald-900" >
+      {isLoggedIn && (
+        <div>
+          server side: {username}, client side:{" "}
+          {auth.user?.profile.preferred_username}
+        </div>
+      )}
+      {!isLoggedIn && (
+        <button onClick={() => auth.signinRedirect()}>Login</button>
+      )}
+      {isLoggedIn && (
+        <button onClick={() => auth.signoutRedirect()}>Logout</button>
+      )}
     </div>
   );
 }
